f6669ef3f4007e6e77cc0c39c72e56c27af36d55
leahzeisner <zeisner.l@northeastern.edu>
2021-05-30 22:26:26 -0400

    modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
+require('dotenv').config();
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
