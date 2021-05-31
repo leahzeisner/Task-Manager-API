f2ceab1a18e6195bae147fc9f5da5fa986b4bdd0
leahzeisner <zeisner.l@northeastern.edu>
2021-05-30 22:31:06 -0400

    modified 3

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
 })
 
 
